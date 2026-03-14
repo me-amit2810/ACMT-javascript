673c1d11cabf32f05e3c02951dc5b3eb6b071917
Amit Kumar <120156579+me-amit2810@users.noreply.github.com>
2026-03-14 13:36:07 +0530

    first commit

---
+++ 01_Basic.js
@@ -0,0 +1,25 @@
+// console.log("Hello, Duniya")  print karne ka function
+
+// console.table([1,2,3,"a","b"]) // data ko table ke format me print krne ke liye
+
+let a = 5; // value can be change later
+console.log(a);
+
+a = 9; // value can be changed
+console.log(a);
+// const b = 6; // value can not be change
+b = 8; // cannot be change
+console.log(b);
+var c = 7; // can be change but the var is too old so nobody use it
+
+d = 8; // not a best practice, reduce the code redability
+// = assignment operator, == : equal to
+
+let accountId; //undefined aayega agar variable me value nhi hai to
+console.log(accountId);
+
+console.log(2+5)
+
+console.log(a,b)
+
+
