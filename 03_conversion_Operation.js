61fae19941488bdbc456e0aae9e17304a43d8fe4
Amit Kumar <120156579+me-amit2810@users.noreply.github.com>
2026-04-01 14:03:51 +0530

    updated

--- 03_conversion_Operation.js
+++ 03_conversion_Operation.js
@@ -27,13 +27,72 @@ let n = '9'; // ye ek string hai
 
 
 let m = "134jk";
-console.log(typeof m)
+// console.log(typeof m)
 
 let v = Number(m);
-console.log(typeof v) // ye number me change to ho jaega but value NaN aayegi matlab Not a number.
+// console.log(typeof v) // ye number me change to ho jaega but value NaN aayegi matlab Not a number.
 
-console.log(v)
+// console.log(v)
 // NaN: Not a number
+
+
+
+// String() => Ye ek function hai jo data ko string me convert krta hai.
+
+
+let Var2 = 78;
+// console.log(Var2);
+
+// console.log(typeof Var2)
+
+let Var3 = String(Var2);
+// console.log(Var3)
+// console.log(typeof Var3)
+
+
+// *************** Operations ****************
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Namaste";
+let str2 = " Duniya";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("2"+3);
+// console.log("2"+4+5);
+// // agar pehli value string hai to sab kuchh as a string treat hoga.
+// console.log(1+3+"6")
+
+// ++  increament value by 1;
+
+let gameCounter = 100;
+
+// console.log(++gameCounter)
+// console.log(gameCounter++)
+// console.log(gameCounter);
+
+// - ye operator kisi bhi value ko negative bna deta hai 
+let negvalue  = -gameCounter;
+// console.log(negvalue);
+
+// Boolean Converstion
+
+// Boolean() -> kisi bhi value ko boolean me convert krne ke liye 
+let user = 0;
+let booleanValue = Boolean(user);
+console.log(typeof booleanValue);
+console.log(booleanValue);
+
+// true => 1; false=> 0;
+
 
 
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-console.log(typeof m)
+console.log(booleanValue);
