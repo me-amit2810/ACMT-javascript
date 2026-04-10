0d7074791a04996c277a274f197c1164ec9d099f
Amit Kumar <120156579+me-amit2810@users.noreply.github.com>
2026-04-10 13:58:07 +0530

    array2

--- 02Array/01_array.js
+++ 02Array/01_array.js
@@ -46,8 +46,32 @@ let myarr = ["a","b","c"]
 
 myarr2 = myarr.join()
 
-console.log(myarr2);
-console.log(typeof  myarr2);
+// console.log(myarr2);
+// console.log(typeof  myarr2);
+
+
+// Slice and Splice
+
+let array5 = [1,2,3,4,5,6,7]  
+
+// Slice(): array ka ek hissa (slice) nikal kr ek new array deta hai.
+// original array me koi change nhi hota
+// Slice(a,b) : a include hoga but b include nhi hoga(end index ko slice nhi krta) 
+
+slice_Arr = array5.slice(0,-1)
+console.log("reverse:",slice_Arr);
+
+// `splice(startIndex, deleteCount)`:
+// Original array se elements ko hata deta hai (ya add bhi kar sakta hai).
+// Ye original array ko modify kar deta hai.
+const myn2 = array5.splice(1, 3) // Index 1 se start karke 3 elements hata do.
+console.log("C ", array5); // `MyArr` ab badal chuka hai.
+
+console.log(myn2); // `myn2` me hataye gaye elements aa jate hain.
+
+
+
+
 
 
 
---
+++ 02Array/02_array.js
@@ -0,0 +1,42 @@
+const marvel_heros = ["thor", "Ironman", "Spiderman"]
+const dc_heros = ["Superman","flash","Batman"]
+
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][0]);
+
+// `concat` method do arrays ko jodkar ek naya array return karta hai. Original arrays change nahi hote.
+
+// let all_heroes = marvel_heros.concat(dc_heros)
+
+// console.log(all_heroes);
+
+
+// Spread Operator (...): Ye `concat` ka naya aur behtar alternative hai.
+// Ye dono arrays ke saare elements ko alag-alag karke ek naye array me daal deta hai.
+
+// all_heroes = [...dc_heros, ...marvel_heros]
+// console.log(all_heroes);
+
+// `flat(depth)` method nested arrays (array ke andar array) ko ek single array me convert karta hai.
+// `depth` batata hai ki kitne level tak flatten karna hai. `Infinity` sabhi levels ko flatten kar dega.
+
+const another_Arr = [1,2,3, [4,5,6], 7, [6, 7, [4, 5]]]
+
+siingleArr = another_Arr.flat(Infinity)
+console.log(siingleArr);
+
+// `Array.isArray()` check karta hai ki di gayi value ek array hai ya nahi.
+console.log(Array.isArray("Amit Kumar")); // Output: false
+
+// `Array.from()` kisi bhi cheez se (jaise string) ek naya array bana deta hai.
+console.log(Array.from("Amit Kumar")); // Output: ['A', 'm', 'i', 't', ' ', 'K', 'u', 'm', 'a', 'r']
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+// `Array.of()` alag-alag elements se ek naya array bana deta hai.
+console.log(Array.of(score1, score2, score3)) // Output: [100, 200, 300]
+
